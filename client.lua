1d5209b9feac8d8c197de01532da55e24828e3d4
DignityDC <98825734+DignityDC@users.noreply.github.com>
2024-05-31 14:23:01 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,32 +1,72 @@
+local me = PlayerPedId()
+local lastPos
+local shakeGameplayCam = false
+
 Citizen.CreateThread(function()
 	local shake_counter1 = 0
 	local shake_counter2 = 0
-
+    
 	while true do
-		Citizen.Wait(0)
-	
-		if IsPlayerFreeAiming(PlayerId()) and IsPedStill(GetPlayerPed(-1)) and not IsPedAimingFromCover(GetPlayerPed(-1)) then
-			Citizen.Wait(100)
-			shake_counter1 = shake_counter1 + 1
-			shake_counter2 = 0
-			if (shake_counter1 == 5) then 
-				ShakeGameplayCam('HAND_SHAKE', Config.shakestill) 
-			end
-		elseif IsPlayerFreeAiming(PlayerId()) and not IsPedStill(GetPlayerPed(-1)) then
-			Citizen.Wait(10)
-			shake_counter2 = shake_counter2 + 1
-			shake_counter1 = 0
-			if (shake_counter2 == 5) then 
-				ShakeGameplayCam('HAND_SHAKE', Config.shakemovement) 
-			end
-		elseif IsPedAimingFromCover(GetPlayerPed(-1)) then
-			StopGameplayCamShaking(true)
-			shake_counter1 = 0
-			shake_counter2 = 0
-		else
-			shake_counter1 = 0
-			shake_counter2 = 0
-			StopGameplayCamShaking(true)
-		end
+		Citizen.Wait(100)
+        if IsPlayerFreeAiming(PlayerId()) then   
+            if IsPedAimingFromCover(me) then
+                    Citizen.Wait(100)
+                    shake_counter1 = shake_counter1 + 1
+                    shake_counter2 = 0
+                    if (shake_counter1 == 5) then 
+                        ShakeGameplayCam('HAND_SHAKE', Config.shakecover)
+                        shakeGameplayCam = true
+                    end
+                    if Config.debug then   
+                        print("shakecover")
+                    end  
+            else
+                if checkPlayerMovement() then
+                    Citizen.Wait(10)
+                    shake_counter2 = shake_counter2 + 1
+                    shake_counter1 = 0
+                    if (shake_counter2 == 5) then 
+                        ShakeGameplayCam('HAND_SHAKE', Config.shakemovement)
+                        shakeGameplayCam = true 
+                    end
+                    if Config.debug then   
+                        print("shakemovement")
+                    end  
+                else
+                    Citizen.Wait(100)
+                    shake_counter1 = shake_counter1 + 1
+                    shake_counter2 = 0
+                    if (shake_counter1 == 5) then 
+                        ShakeGameplayCam('HAND_SHAKE', Config.shakestill)
+                        shakeGameplayCam = true  
+                    end
+                    if Config.debug then   
+                        print("shakestill")
+                    end 
+                end
+            end    
+        else
+            if shakeGameplayCam then 
+                shake_counter1 = 0
+                shake_counter2 = 0
+                StopGameplayCamShaking(true) 
+            end    
+        end     
 	end
 end)
+
+function checkPlayerMovement()
+    local isMoving
+    local currentPos = GetEntityCoords(me)
+    if lastPos ~= nil then
+        isMoving = (currentPos ~= lastPos)
+    end
+    --Citizen.Wait(100)
+    lastPos = currentPos
+
+    if isMoving then
+        return true 
+    else
+        return false
+    end
+end
